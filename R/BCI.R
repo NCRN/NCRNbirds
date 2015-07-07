8edd9bab5dae1db30a3c10e775d31807226931e9
johnpauls <johnpauls@operamail.com>
2015-07-07 16:21:58 -0400

    V 0.1.6.1  More work on BCI, fixed error in getGuildsdocs, did not remake docs or package

--- R/BCI.R
+++ R/BCI.R
@@ -4,9 +4,8 @@
 #' 
 #' @description Calcualtes the Bird Conservation Index (BCI)
 #' 
-#' @importFrom dplyr group_by left_join mutate select summarize
+#' @importFrom dplyr mutate rowwise
 #' @importFrom magrittr %>%
-#' @importFrom tidyr spread 
 #' 
 #' 
 #' @param object An \code{NCRNbirds} object or a \code{list} of such objects.
@@ -44,11 +43,81 @@ setMethod(f="BCI", signature=c(object="list"),
 setMethod(f="BCI", signature=c(object="NCRNbirds"),
           function(object,points,AOU,years,band,visits,output,...){
             XPoints<-getPoints(object=object,years=years)$Point_Name
-            XList<-sapply(X=XPoints, FUN=getChecklist, object=object,years=years)
-            XBCI<-as.data.frame(matrix(nrow=length(XList), ncol=5))
-            
-            # use the @Species slot to add the guilds to the birds
-            # use mutates to get the numbers
+            XList<-lapply(X=XPoints, FUN=getChecklist, object=object,years=years)
+            XGuilds<-getGuilds(object=object)
+            XBCI<-data.frame(Point_Name=XPoints)
+             XBCI<-XBCI %>% rowwise %>% 
+               mutate(ForestGeneralist= sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Primary.Habitat=="Forest Generalist",]$AOU_Code),
+                      InteriorForest=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Primary.Habitat=="Interior Forest Obligate",]$AOU_Code),
+                      ForestGroundNester=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Nest.Placement=="Forest Ground Nester",]$AOU_Code),
+                      OpenGroundNester=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Nest.Placement=="Open Ground Nester",]$AOU_Code),
+                      ShrubNester=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Nest.Placement=="Shrub",]$AOU_Code),
+                      CanopyNester=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Nest.Placement=="Canopy",]$AOU_Code),
+                      BarkProber=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Foraging.Behavior=="Bark Prober" & 
+                                                                        XGuilds$Trophic.Level=="Insectivore",]$AOU_Code),
+                      GroundGleaner=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Foraging.Behavior=="Ground Gleaner" & 
+                                                                        XGuilds$Trophic.Level=="Insectivore",]$AOU_Code),
+                      CanopyInsectivore=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Foraging.Behavior=="Upper Canopy Forager" & 
+                                                                          XGuilds$Trophic.Level=="Insectivore",]$AOU_Code),
+                      ShrubInsectivore=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Foraging.Behavior=="Lower Canopy Forager" & 
+                                                                          XGuilds$Trophic.Level=="Insectivore",]$AOU_Code),
+                      Omnivore=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Trophic.Level=="Omnivore",]$AOU_Code),
+                      NestPredator=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Pred_Para_Desc=="Yes",]$AOU_Code),
+                      Exotic=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Exotic=="Exotic",]$AOU_Code),
+                      Resident=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Migratory=="Resident",]$AOU_Code),
+                      TemperateMigrant=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Migratory=="Temperate Migrant",]$AOU_Code),
+                      SingleBrooded=sum(XList[[Point_Name]]%in% XGuilds[XGuilds$Brood.Numbers=="Single Brooded",]$AOU_Code),
+                      Total=length(XList[[Point_Name]])
+                ) 
+             XBCI<-XBCI %>% 
+               mutate(Pro_ForestGeneralist=round(ForestGeneralist/Total,digits=3),
+                      Pro_InteriorForest=round(InteriorForest/Total,digits=3),
+                      Pro_ForestGroundNester=round(ForestGroundNester/Total,digits=3),
+                      Pro_OpenGroundNester=round(OpenGroundNester/Total,digits=3),
+                      Pro_ShrubNester=round(ShrubNester/Total,digits=3),
+                      Pro_CanopyNester=round(CanopyNester/Total,digits=3),
+                      Pro_BarkProber=round(BarkProber/Total,digits=3),
+                      Pro_GroundGleaner=round(GroundGleaner/Total,digits=3),
+                      Pro_CanopyInsectivore=round(CanopyInsectivore/Total,digits=3),
+                      Pro_ShrubInsectivore=round(ShrubInsectivore/Total,digits=3),
+                      Pro_Omnivore=round(Omnivore/Total,digits=3),
+                      Pro_NestPredator=round(NestPredator/Total,digits=3),
+                      Pro_Exotic=round(Exotic/Total, digits=3),
+                      Pro_Resident=round(Resident/Total,digits=3),
+                      Pro_TemperateMigrant=round(TemperateMigrant/Total,digits=3),
+                      Pro_SingleBrooded=round(SingleBrooded/Total,digits=3)
+                )
+             
+             XBCI<-XBCI %>% 
+               mutate(BCI_ForestGeneralist=c(4.5, 2.5)[findInterval(Pro_ForestGeneralist,vec=c(0, 0.281, 1.001))],
+                      BCI_InteriorForest=c(1, 1.5, 3, 4, 5)[findInterval(Pro_InteriorForest, vec=c(0,0.011,0.081,0.261,0.431,1.001))],
+                      BCI_ForestGroundNester=c(1, 1.5, 3, 4.5, 5)[findInterval(Pro_ForestGroundNester, vec=c(0,0.001,0.021,0.161,0.241,1.001))],
+                      BCI_OpenGroundNester=c(1, 2.5, 5)[findInterval(Pro_OpenGroundNester, vec=c(0,0.021,0.111,1.001))],
+                      BCI_ShrubNester=c(4, 1.5, 1)[findInterval(Pro_ShrubNester, vec=c(0,0.211,0.331,1.001))],
+                      BCI_CanopyNester=c(1.5, 2, 4.5)[findInterval(Pro_CanopyNester, vec=c(0,0.281,0.321,1.001))],
+                      BCI_BarkProber=c(1.5, 3, 4, 5)[findInterval(Pro_BarkProber, vec=c(0,0.061,0.111,0.171,1.001))],
+                      BCI_GroundGleaner=c(1.5, 2, 4.5, 5)[findInterval(Pro_GroundGleaner, vec=c(0,0.051,0.071,0.141,1.001))],
+                      BCI_CanopyInsectivore=c(1.5, 2, 3, 4.5, 5)[findInterval(Pro_CanopyInsectivore, vec=c(0,0.031,0.051,0.121,0.201,1.001))],
+                      BCI_ShrubInsectivore=c(1.5, 2.5, 5)[findInterval(Pro_ShrubInsectivore, vec=c(0,0.141,0.231,1.001))],
+                      BCI_Omnivore=c(5, 4, 3, 1, 2)[findInterval(Pro_Omnivore, vec=c(0,0.291,0.411,0.481,0.581,1.001))],
+                      BCI_NestPredator=c(5, 3.5, 2, 1)[findInterval(Pro_NestPredator, vec=c(0,0.101,0.151,0.181,1.001))],
+                      BCI_Exotic=c(5, 4.5, 3, 2, 1)[findInterval(Pro_Exotic, vec=c(0,0.001,0.021,0.051,0.111,1.001))],
+                      BCI_Resident=c(5, 3.5, 2, 1)[findInterval(Pro_Resident, vec=c(0,0.261,0.391,0.571,1.001))],
+                      BCI_TemperateMigrant=c(4, 2, 1)[findInterval(Pro_TemperateMigrant, vec=c(0,0.211,0.301,1.001))],
+                      BCI_SingleBrooded=c(1.5, 2, 3, 4, 5)[findInterval(Pro_SingleBrooded, vec=c(0,0.411,0.451,0.611,0.731,1.001))]
+                )
+             XBCI<-XBCI %>% 
+               mutate(BCI_Structural=BCI_ForestGeneralist + BCI_InteriorForest + BCI_ForestGroundNester + BCI_OpenGroundNester + BCI_ShrubNester + 
+                        BCI_CanopyNester,
+                      BCI_Funcitonal=BCI_BarkProber + BCI_GroundGleaner + BCI_CanopyInsectivore + BCI_ShrubInsectivore + BCI_Omnivore,
+                      BCI_Compostional=BCI_NestPredator + BCI_Exotic + BCI_Resident + BCI_TemperateMigrant + BCI_SingleBrooded,
+                      BCI = BCI_Structural + BCI_Funcitonal + BCI_Compostional,
+                      BCI_Category=c("Low Integrity", "Meidum Integrity","High Integrity","Highest Integrity")[findInterval(BCI,
+                                                                                                                    vec=c(0,40.1,52.1,60.1,77.1))]
+                )
+             
+                 
+             
             # add them up and spit out park, point, year, BCI, category, category codes as a data.frame
             return(XBCI)
             ## Multiple years?
--- R/getGuilds.R
+++ R/getGuilds.R
@@ -8,7 +8,7 @@
 #' 
 #' @param object An \code{NCRNbirds} object or a list of such objects.
 #' @param AOU, an optional character vector of AOU codes. Each code should be in quotes. Only data from those birds will be returned.
-#' @param guilds A character vector of guild categories. Only data for those categories will be returned. Options are:
+#' @param categories A character vector of guild categories. Only data for those categories will be returned. Options are:
 #' \describe{
 #' \item{"Migratory"}{Migratory status of the species}
 #' \item{"Brood.Numbers"}{Is the speices single or double brooded}
@@ -18,6 +18,7 @@
 #' \item{"Foraging.Behavior"}{What tyoe of foraging behavior does the species show? Possibilites include wate plunger, bark prober,upper canopy forager, etc.}
 #' \item{"Tropic.Level"}{What does the species feed on? Can be omnivore, carnivore, insectivore etc.}
 #' \item{"Nest.Height"}{How high does the species nest? High, low or highly variable.}
+#' \item{"Pred_Para_Desc"}{Is the species a nest predator or brood parasite.Yes or no.}
 #' }
 #' @param output Either "dataframe" (the default) or "list". Note that this must be in quotes. Determines the type of output from the function. When a list of \code{NCRNbirds} object is used, and the output is "dataframe" then a each unique conbination of species and guidls will only be listed once in the resulting dataframe. If some species have ifferent guild assingments in the different \code{NCRNbirds} objects in the list, then those species will be listed twice which could have unintended consiquences
 #' 
@@ -47,9 +48,10 @@ setMethod(f="getGuilds", signature=c(object="NCRNbirds"),
             if(!anyNA(AOU)) XGuilds<-XGuilds[XGuilds$AOU %in% AOU,]
             
             if(!anyNA(categories)) GNames<-c("AOU_Code",categories) else GNames<-c("AOU_Code", "Migratory", "Brood.Numbers", "Primary.Habitat",
-                                                                        "Exotic", "Nest.Placement", "Foraging.Behavior","Trophic.Level", "Nest.Height")
+                                                                        "Exotic", "Nest.Placement", "Foraging.Behavior","Trophic.Level",
+                                                                        "Nest.Height","Pred_Para_Desc")
             
             XGuilds<-XGuilds %>% dplyr::select(which(colnames(XGuilds) %in% GNames))
             
-            return(XGuilds[,])
+            return(XGuilds)
 })
