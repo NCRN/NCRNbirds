b82e62ddf86b111d67e497947ea4f2d32c7d3cfb
johnpauls <johnpauls@operamail.com>
2015-07-13 14:05:08 -0400

    v 0.1.8
    Added getParkNames

---
+++ R/getParkNames.R
@@ -0,0 +1,33 @@
+#' @include NCRNbirds_Class_def.R 
+#' @title getNParkNames
+#' 
+#' @description Retreives park names from an NCRNbirds object or a list of such objects.
+#' 
+#' @param object Either an NPSbirds object or a list of such objects.
+#' @param name.class Type of name to return. One of three options, in quotes.
+#' \describe{
+#' \item{"short"}{ The default. Returns the short name of the park}
+#' \item{"long"}{Returns the long name of the park}
+#' \item{"code"}{Returns the park code}
+#' } 
+#' @return A character vector with one or more park names. 
+#' 
+#' @export
+
+setGeneric(name="getParkNames",function(object,name.class="short"){standardGeneric("getParkNames")},signature=c("object") )
+
+
+setMethod(f="getParkNames", signature=c(object="list"),
+          function(object,name.class){
+            sapply(object,FUN=getParkNames, name.class=name.class) } )  
+
+
+setMethod(f="getParkNames", signature=c(object="NCRNbirds"),
+          function(object,name.class){
+            switch(name.class,
+                   code = return(object@ParkCode),
+                   short = return(object@ShortName),
+                   long = return(object@LongName),
+                   stop("Unrecognized type in getNames")
+            )
+          })
