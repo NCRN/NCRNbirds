bcc89dfc50f6499514c35757a6bea697b0802cfe
johnpauls <johnpauls@operamail.com>
2018-03-28 12:56:06 -0400

    v 0.2.20
    - now CountXVisit should use the visits slot which will make the visualizer more easily account for diffreetn number of visits
    
    - closes #19
    - fixes #16

--- R/CountXVisit.R
+++ R/CountXVisit.R
@@ -1,4 +1,4 @@
-#' @include NCRNbirds_Class_def.R getVisits.R getBirds.R 
+#' @include NCRNbirds_Class_def.R getVisits.R getBirds.R getDesign.R
 #' 
 #' @title CountXVisit
 #' 
@@ -7,7 +7,7 @@
 #' @importFrom dplyr group_by left_join mutate select summarize ungroup bind_rows
 #' @importFrom magrittr %>%
 #' @importFrom tidyr spread 
-#' 
+#' @importFrom data.table rbindlist
 #' 
 #' @param object An \code{NCRNbirds} object or a \code{list} of such objects.
 #' @param points A character vector. The names of one or more points where the data was collected.
@@ -15,19 +15,26 @@
 #' @param years  A vector of numbers. will return only data from the indicated years.
 #' @param times  A numeric vector of length 1 passed on to \code{link{getVisits}}. Returns only data from points where the number of years that a point has been visited is greater or equal to the value of \code{times}. This is determined based on the data found in the \code{Visits} slot.
 #' @param band. A numeric vector. Defaults to 1. Only observations whose \code{Distance_id} field matches a value in \code{band} will be returned.
-#' @param visits  The visits that will be used for the matrix. Defautls to \code{c(1,2)}.
+#' @param visits  The visits that will be used for the matrix. Defautls to \code{NA}. See Details below,
 #' @param output Either "dataframe" (the default) or "list". Note that this must be in quotes. Determines the type of output from the function.
 #' @param ... Additional arguments passed to \code{getBirds}
 #' 
-#' @details This produces a Count X Visit matrix for a \code{NCRNbirds} object or a \code{list} of such objects. Each row of the matrix will correspond to a different pointt in a different year. The columns of the matrix will be the park code, the point name, the year visited, and a column of abundances of the indcated species at that visit. If multiple species are indicated in \code{AOU}, their abundances will be totaled. 
-#' 
+#' @details This produces a Count X Visit matrix for a \code{NCRNbirds} object or a \code{list} of such objects. Each row of the matrix
+#'  will correspond to a different pointt in a different year. The columns of the matrix will be the park code, the point name, the year 
+#'  visited, and a column of abundances of the indcated species at that visit. If multiple species are indicated in \code{AOU}, their 
+#'  abundances will be totaled. 
+#'  
+#'  If \code{visits} is left as \code{NA} then the visits used will be 1 through the number of visits indicated in the \code{visits} slot. 
+#'  Otherwise a numeric vectore e.g. c(1,2) can be used to select which visits are used. 
+#'     
 #' @export
 
 
 ########################
 
 
-setGeneric(name="CountXVisit",function(object,points=NA,AOU=NA,years=NA,times=NA,band=1,visits=c(1,2),output="dataframe",...){standardGeneric("CountXVisit")}, signature="object")
+setGeneric(name="CountXVisit",function(object,points=NA,AOU=NA,years=NA,times=NA,band=1,visits=NA,
+                                       output="dataframe",...){standardGeneric("CountXVisit")}, signature="object")
 
 
 
@@ -36,7 +43,7 @@ setMethod(f="CountXVisit", signature=c(object="list"),
             OutMat<-lapply(X=object, FUN=CountXVisit, points=points, AOU=AOU, years=years, times=times,band=band,visits=visits,...)
             switch(output,
                    list= return(OutMat),
-                   dataframe=return(bind_rows(OutMat))
+                   dataframe= return(rbindlist(OutMat, use.names=TRUE, fill=TRUE)) #return(bind_rows(OutMat))
             )
           })
 
@@ -46,6 +53,7 @@ setMethod(f="CountXVisit", signature=c(object="NCRNbirds"),
             
             ## This makes a matrix with 1 for visits that occured and NA for visits that did not occur (such as only
             ##  visiting a point once instead of twice)
+            Visits<-if(is.na(visits)) 1:getDesign(object,info="visits") else visits
             
             VisitMat<-getVisits(object=object,points=points,years=years,times=times,visits=visits) %>%
               mutate(Visit=paste0("Visit",Visit),Visited=1) %>%
