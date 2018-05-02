a8a89296888e6a90797b5b5d176b31edf1064c5e
aaronweed <aaron.weed@gmail.com>
2018-05-02 13:39:06 -0400

    added design elements to the import function (intevrals, bands, and visits)

--- R/importMIDNbirds.R
+++ R/importMIDNbirds.R
@@ -2,28 +2,33 @@
 #' 
 #' @title importMIDNbirds
 #' 
-#' @importFrom lubridate year
+#' @importFrom lubridate year mdy
 #' 
 #' @description  This function imports data from the standard MIDN .csv files and saves it as \code{NCRNbirds} objects. The required .csv files are: Points, Visits, FieldData and BirdGuildAssignments.
 #' 
 #' @param Dir  The directory where the data is found. You should omit the trailing slash ("/") in the directory name.
 #' 
-#' @return Returns a list of 11 \code{NCRNbirds} objects, one for each park.
+#' @return Returns a list of 12 \code{NCRNbirds} objects, one for each park.
 #' 
 #' @export
 
 
 importMIDNbirds<-function(Dir){
+
+  
+  InBands<-read.csv(paste(Dir,"MIDNbands.csv", sep="/"),as.is=T, header = T)
+  
+  InIntervals<-read.csv(paste(Dir,"MIDNintervals.csv", sep="/"),as.is=T, header = T)
   
   InPoints<-read.csv(paste(Dir,"Points.csv", sep="/"),as.is=T, header=T)
   
   InVisits<-read.csv(paste(Dir,"Visits.csv",sep="/"),as.is=T, header=T)
-  InVisits$EventDate<-as.Date(as.character(InVisits$EventDate), format="%m/%d/%Y")
+  InVisits$EventDate<-mdy(InVisits$EventDate)
   InVisits$Year<-year(InVisits$EventDate)
-  
+  InVisits$Visit<-as.character(InVisits$Visit)
   
   InFieldData<-read.csv(paste(Dir,"FieldData.csv", sep="/"),as.is=T, header=T)
-  InFieldData$EventDate<-as.Date(as.character(InFieldData$EventDate), format="%m/%d/%Y")
+  InFieldData$EventDate<-mdy(InFieldData$EventDate)
   InFieldData$Year<-year(InFieldData$EventDate)
   
   InSpecies<-read.csv(paste(Dir,"BirdGuildAssignments.csv", sep="/"), as.is=T, header=T )
@@ -34,11 +39,15 @@ importMIDNbirds<-function(Dir){
             LongName="Appomattox Court House National Historical Park", 
             Network="MIDN", 
             
+            VisitNumber=2,
+            Bands=InBands,
+            Intervals=InIntervals,
+            
             Points=InPoints[InPoints$Admin_Unit_Code=="APCO",], 
             Visits=InVisits[InVisits$Admin_Unit_Code=="APCO",],
             Birds=InFieldData[InFieldData$Admin_Unit_Code=="APCO",],
             Species=InSpecies
-            )
+  )
   
   
   BOWA<-new("NCRNbirds", 
@@ -46,6 +55,10 @@ importMIDNbirds<-function(Dir){
             ShortName="Booker T.", 
             LongName="Booker T. Washington National Monument", 
             Network="MIDN", 
+            
+            VisitNumber=2,
+            Bands=InBands,
+            Intervals=InIntervals,
             
             Points=InPoints[InPoints$Admin_Unit_Code=="BOWA",], 
             Visits=InVisits[InVisits$Admin_Unit_Code=="BOWA",],
@@ -59,6 +72,10 @@ importMIDNbirds<-function(Dir){
             LongName="Fredericksburg and Spotsylvania National Military Park", 
             Network="MIDN", 
             
+            VisitNumber=2,
+            Bands=InBands,
+            Intervals=InIntervals,
+            
             Points=InPoints[InPoints$Admin_Unit_Code=="FRSP",], 
             Visits=InVisits[InVisits$Admin_Unit_Code=="FRSP",],
             Birds=InFieldData[InFieldData$Admin_Unit_Code=="FRSP",],
@@ -70,7 +87,11 @@ importMIDNbirds<-function(Dir){
             ShortName="Petersburg", 
             LongName="Petersburg National Battlefield", 
             Network="MIDN", 
-             
+            
+            VisitNumber=2 ,
+            Bands=InBands,
+            Intervals=InIntervals,
+            
             Points=InPoints[InPoints$Admin_Unit_Code=="PETE",], 
             Visits=InVisits[InVisits$Admin_Unit_Code=="PETE",],
             Birds=InFieldData[InFieldData$Admin_Unit_Code=="PETE",],
@@ -82,6 +103,10 @@ importMIDNbirds<-function(Dir){
             ShortName="Richmond", 
             LongName="Richmond National Battlefield Park", 
             Network="MIDN", 
+            
+            VisitNumber=2,
+            Bands=InBands,
+            Intervals=InIntervals,
             
             Points=InPoints[InPoints$Admin_Unit_Code=="RICH",], 
             Visits=InVisits[InVisits$Admin_Unit_Code=="RICH",],
@@ -95,12 +120,16 @@ importMIDNbirds<-function(Dir){
             LongName="Valley Forge National Historical Park", 
             Network="MIDN", 
             
+            VisitNumber=2,
+            Bands=InBands,
+            Intervals=InIntervals,
+            
             Points=InPoints[InPoints$Admin_Unit_Code=="VAFO",], 
             Visits=InVisits[InVisits$Admin_Unit_Code=="VAFO",],
             Birds=InFieldData[InFieldData$Admin_Unit_Code=="VAFO",],
             Species=InSpecies
   ) 
   
-
+  
   return(c(APCO,BOWA,FRSP,PETE,RICH,VAFO))
 }
