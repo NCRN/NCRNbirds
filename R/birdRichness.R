504405cb41a88d5f73774b19d266843f138675ba
johnpauls <johnpauls@operamail.com>
2018-05-03 11:21:17 -0400

    Version 0.2.22
    - added if_else as a dplyr import in birdRichenss()
    
    - updates to DESCRIPTION
    
    - changed birdRichness() so if visits were made to a site but no birds were found richness =0, but if no visits were made richness=NA
    
    - updated birdRichess() help

--- R/birdRichness.R
+++ R/birdRichness.R
@@ -2,18 +2,27 @@
 #' 
 #' @title birdRichness
 #' 
-#' @importFrom dplyr n_distinct
+#' @importFrom dplyr if_else n_distinct
 #' 
 #' @description Returns the number of bird species found in a park, at a point or a collection of points. 
 #' 
 #' @param object An NCRNbirds object or a list of such objects.
-#' @param points A character vector passed to \code{\link{getBirds}}. The names of one or more points where the data was collected. \code{NA}(the default) meausures richness from all points.
-#' @param AOU  A character vector passed to \code{\link{getBirds}}. One or more AOU (American Ornothological Union) codes of bird species. \code{NA} (the default) measures richness based on all species, otherwise only species listed in \code{AOU} will count.
-#' @param years  A vector of numbers passed to \code{\link{getBirds}}. \code{NA} (the default) will count species regardless of the year they were monitored, otherwise only data from years listed in \code{years} will be used.
-#' @param output Either "total" (the default) or "list". Note that this must be in quotes. Determines the type of output from the function, when \code{object} is a \code{list}. "total" will give the number of distinct species found across all parks. "List" will return a list, with each entry to the list corresponding to the species richness of one of the \code{NCRNbirds} objects in the input list.  
+#' @param points A character vector passed to \code{\link{getBirds}}. The names of one or more points where the data was collected.
+#'  \code{NA}(the default) measures richness from all points.
+#' @param AOU  A character vector passed to \code{\link{getBirds}}. One or more AOU (American Ornothological Union) codes of bird species. 
+#' \code{NA} (the default) measures richness based on all species, otherwise only species listed in \code{AOU} will be counted.
+#' @param years  A vector of numbers passed to \code{\link{getBirds}}. \code{NA} (the default) will count species regardless of the year
+#'  they were monitored, otherwise only data from years listed in \code{years} will be used.
+#' @param output Either "total" (the default) or "list". Note that this must be in quotes. Determines the type of output from the function, 
+#' when \code{object} is a \code{list}. "total" will give the number of distinct species found across all parks. 
+#' "list" will return a list, with each entry to the list corresponding to the species richness of one of the \code{NCRNbirds} objects in the input list.  
 #' @param ... additional arguments passed to \code{\link{getBirds}}. Any argument which is a valid argument for \code{\link{getBirds}} can be used here.
 #' 
-#' @details This function calculates the species richness for a park, group of parks, group of monitoring points etc. Species richness is the number of different species found, regardless of their abundance. The function works by first getting the monitoring data by using the \code{\link{getBirds}} function and then counting the number of unique birds found. All of the options for combining or subsetting data in \code{\link{getBirds}} will work for \code{birdRichness} including subsetting by year and monitoring point. 
+#' @details This function calculates the species richness for a park, group of parks, group of monitoring points etc. 
+#' Species richness is the number of different species found, regardless of their abundance. The function works by first getting the monitoring data by 
+#' using the \code{\link{getBirds}} function and then counting the number of unique birds found. All of the options for combining or subsetting 
+#' data in \code{\link{getBirds}} will work for \code{birdRichness} including subsetting by year and monitoring point. If there is no data, due to 
+#' no sampling occuring at hte specified time and place, \code{NA} will be returned. 
 #' 
 #' @export
 
@@ -35,6 +44,6 @@ setMethod(f="birdRichness", signature=c(object="list"),
 setMethod(f="birdRichness", signature=c(object="NCRNbirds"),
           function(object,points,AOU,years,output,...){
             count<- n_distinct(getBirds(object=object,points=points,AOU=AOU,years=years,output=output,...)$AOU_Code)
-            if_else(count == 0,NA_integer_,count)
+            if_else(count == 0 & nrow(getVisits(object=object, points=points, years=years))==0, NA_integer_, count)
           }
 )
