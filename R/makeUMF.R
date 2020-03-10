47f7596811a19fa4bd5b72d713ab4bfd8d1bc4da
John Paul Schmit <john_schmit@nps.gov>
2020-03-10 14:58:00 -0400

    v add_Zach_analysis 8 - finished unmarkedBirds for now - updated makeUMF - closes #150 - closes #151 - closes #152 - closes #34

--- R/makeUMF.R
+++ R/makeUMF.R
@@ -5,10 +5,10 @@
 #' 
 #' @description Makes an unmarked frame for analysis using the unmarked package
 #' 
-#' @importFrom dplyr select
+#' @importFrom dplyr bind_cols select transmute
 #' @importFrom magrittr %>%
 #' @importFrom purrr map pmap
-#' @importFrom  unmarked unmarkedFrameOccu unmarkedFramePCount
+#' @importFrom unmarked unmarkedFrameOccu unmarkedFramePCount
 #'
 #' @param frametype Indicates the type of \code{unmarkedFrame} to make. Options are "pcount" the default incudes number of decections for n-mixture modeling. 
 #' "occu" is used for occupancy modeling. 
@@ -22,7 +22,9 @@
 #' by the \code{\link{CovsXVisit}} function.This will be used in conjuciton with the visit covariates specified by \code{visitcovs}.If the input \code{object}
 #' is a \code{list} and the desired output is a \code{list} than \code{obscovslist} should also be a \code{list} of \code{lists}
 #' with one \code{list} of observation covariates for each park.
-#' 
+#' @param trend Used for multi-year data sets to indicate if the year of obsevation shoud be included in the model. Options are "none" the default, "numeric"
+#' to indicate that year should be treated as a numeric value and "factor" to indicate that year should be treated as a factor. Note that is the trend is set
+#' to "numeric" it is centered to imporve the odds of the model successfully fitting.
 #' @param output   Either "umf" (the defautlt) or "list". Controls the output when \code{object} is a list. If "umf" the data from all \code{NCRNbirds}
 #' objects in the list will be combined into a single unmarked frame. If "list" then the output will be a list of unmarked frames - one for 
 #' each \code{NCRNbirds} object in the input list. 
@@ -30,7 +32,10 @@
 #' 
 #' @return Either an \code{unmarkedFrameOccu} or an \code{unmarkedFramePcount} object. 
 #' 
-#' @details This function simplifies the process for fitting a N-mixture model (Royal 2004, Kery et al. 2005) to data in an NCRNbirds object or a list of such objects. The data is first extracted from the object(s) using the \code{\link{CountXVisit}} funciton. The data is then fed to the \code{\link[unmarked]{pcount}} function and the resutls of the analysis are returned. 
+#' @details This function simplifies the process for fitting of occupancy or N-mixture models (Royal 2004, Kery et al. 2005) to data in an NCRNbirds object 
+#' or a list of such objects. The data is first extracted from the object(s) using the \code{\link{CountXVisit}} funciton. The data is then fed to 
+#' the \code{\link[unmarked]{unmarkedFramePCount}} or  \code{\link[unmarked]{unmarkedFrameOccu}}function. The resulting object can then be passed to \
+#' \code{\link{unmarkedBirds}} for analysis. 
 #' 
 #' @references Kery, M. Royle, J.A. and Schmid. 2005. Modeling avian abundance from replicated counts using binomail mixutre models. Ecological Applications. 15: 1450-1461. 
 #' @references Royle, J. A.  2004. N-Mixture models for estimating population size from spatially replicated counts. Biometrics 60: 108-115.
@@ -41,15 +46,15 @@
 ########################
 
 setGeneric(name="makeUMF",function(object, parks=NA, points=NA, AOU=NA, years=NA, times=NA, visits=NA, reps=NA, frametype="pcount",sitecovs=NULL, visitcovs=NA, 
-                                   obscovslist=NULL, output="umf",... ){standardGeneric("makeUMF")},  signature="object")
+                                   obscovslist=NULL, trend="none", output="umf",... ){standardGeneric("makeUMF")},  signature="object")
 
 setMethod(f="makeUMF", signature=c(object="list"),
-  function(object, parks, points, AOU, years, times, visits, reps, frametype, sitecovs, visitcovs, obscovslist, output, ...) {
+  function(object, parks, points, AOU, years, times, visits, reps, frametype, sitecovs, visitcovs, obscovslist, trend, output, ...) {
     switch(output,
       umf= {
             BirdMat<-CountXVisit(object=object, parks=parks, points=points, AOU=AOU, years=years, times=times, visits=visits, reps=reps,
                                  type=switch(frametype, pcount="count", occu="occupancy"),output="dataframe", ...) %>% 
-            dplyr::select(-c(Admin_Unit_Code, Point_Name, Year))
+            dplyr::select(-c(Admin_Unit_Code, Point_Name))
                  
             VisitData<- if(!anyNA(visitcovs)){
               visitcovs %>% map(~CovsXVisit(object, parks=parks, points=points, years=years, times=times, visits=visits, reps=reps, 
@@ -57,14 +62,23 @@ setMethod(f="makeUMF", signature=c(object="list"),
                             select(-Admin_Unit_Code,-Point_Name,-Year)) %>% 
                            setNames(visitcovs)
             }else NULL
-            return(makeUMF(object=BirdMat,frametype=frametype, sitecovs=sitecovs, obscovslist = c(VisitData, obscovslist)))
+            
+            YearMat<-switch(trend,
+                            none=NULL,
+                            numeric=BirdMat %>% transmute(Year=scale(Year,scale=FALSE)),
+                            factor=BirdMat %>% transmute(Year=factor(Year))
+                           )
+            
+          sitecovs<-if(trend!="none") bind_cols(YearMat,sitecovs) else sitecovs #bind_cols used as cbind has issues with NULL
+          
+          return(makeUMF(object=BirdMat %>% select(-Year),frametype=frametype, sitecovs=sitecovs, obscovslist = c(VisitData, obscovslist)))
       },
                
       list={
             umflist<-list(object, sitecovs, obscovslist)
             umflist<-umflist[!sapply(umflist, is.null)] # removes any null elements so pmap will work
-            return(pmap(.l=umflist, makeUMF, parks=parks, points=points, years=years,times=times, visits=visits, reps=reps, frametype=frametype, 
-                        visitcovs=visitcovs) )
+            return(pmap(.l=umflist, makeUMF, parks=parks, points=points,AOU=AOU, years=years,times=times, visits=visits, reps=reps, frametype=frametype, 
+                        visitcovs=visitcovs, trend=trend) )
       }
     )
    }
@@ -72,19 +86,26 @@ setMethod(f="makeUMF", signature=c(object="list"),
  
  
 setMethod(f="makeUMF", signature=c(object="NCRNbirds"),
-    function(object, parks, points, AOU, years, times, visits, reps, frametype, sitecovs, visitcovs, obscovslist, ...){
+    function(object, parks, points, AOU, years, times, visits, reps, frametype, sitecovs, visitcovs, obscovslist, trend,...){
            
       BirdMat<-CountXVisit(object=object, parks=parks, points=points, AOU=AOU, years=years, times=times, visits=visits,
                            reps=reps,  type=switch(frametype, pcount="count", occu="occupancy"), ...) %>% 
-        dplyr::select(-c(Admin_Unit_Code, Point_Name, Year))
+        dplyr::select(-c(Admin_Unit_Code, Point_Name))
       
       VisitData<- if(!anyNA(visitcovs)){
         visitcovs %>% map(~CovsXVisit(object, parks=parks, points=points, years=years, times=times, visits=visits, reps=reps, covs=.x) %>% 
-                      select(-Admin_Unit_Code,-Point_Name,-Year)) %>% 
+                      select(-Admin_Unit_Code,-Point_Name)) %>% 
           setNames(visitcovs)
       }else NULL
- 
-   return(makeUMF(object=BirdMat, frametype=frametype,sitecovs=sitecovs, obscovslist = c(VisitData, obscovslist)))
+      
+      YearMat<-switch(trend,
+                      none=NULL,
+                      numeric=BirdMat %>% transmute(Year=scale(Year,scale=FALSE)),
+                      factor=BirdMat %>% transmute(Year=factor(Year))
+      )
+      sitecovs<-if(trend!="none") bind_cols(YearMat,sitecovs) else sitecovs #bind_cols used as cbind has issues with NULL
+      
+   return(makeUMF(object=BirdMat %>% select(-Year), frametype=frametype,sitecovs=sitecovs, obscovslist = c(VisitData, obscovslist)))
   }
  )
 
--- R/unmarkedBirds.R
+++ R/unmarkedBirds.R
@@ -1,5 +1,4 @@
 #' @include NCRNbirds_Class_def.R 
-#' @inheritParams getBirds
 #' 
 #' @title unmarkedBirds
 #' 
@@ -7,37 +6,22 @@
 #' 
 #' @importFrom dplyr case_when
 #' @importFrom magrittr %>%
-#' @importFrom  unmarked obsCovs occu siteCovs
+#' @importFrom unmarked obsCovs occu pcount siteCovs
 
 #' @importFrom purrr map pmap
 
 
-#' @object An unamked frame object such as those created by \code{makeUMF}. 
+#' @param object An unamked frame object such as those created by \code{makeUMF} or a \code{list} of such frames. 
 #' @param formula A formula for the the model. Formula indicates covariates for observation and detection in that order. i.e. ~Day ~Forest_Type  Note that 
 #' this is NOT in quotes.
+#' @param ... Additonal arguments passed to the relevant unmarked model fitting method. 
+#' @return A fitted unmaked model. Currently fits occupancy and n-mixture models. 
 #' 
-#' 
-#' @param frametype Indicates the type of \code{unmarkedFrame} to make. Options are "pcount" the default incudes number of decections for n-mixture modeling. 
-#' "occu" is used for occupancy modeling. 
-#' @param sitecovs A \code{data.frame} of site covariates created by the user.Each covariate shoulb be in a separate olumn and point in its own row.
-#' This should not include a column of point names. Make sure that the order of the points in this \code{data.frame} math the order in the observation data. 
-#' If the input \code{object} is a \code{list} and the desired output is a \code{list} than \code{sitecovs} should also be a \code{list} of \code{data.frames}
-#' with one \code{data.frame} for each park.
-#' @param visitcovs A characeter vector with names of visit covariates to be used which ARE obtained from the \code{\link{CovsXVisit}} function.\code{makeUMF}
-#' will automtically format these for use in unmarkedFrames. These will be formatted at a list of named \code{data.frames}
-#' @param obscovslist A named \code{list} of \code{data.frame}s with observation covariates. Typically this will include covariates that ARE NOT returned 
-#' by the \code{\link{CovsXVisit}} function.This will be used in conjuciton with the visit covariates specified by \code{visitcovs}.If the input \code{object}
-#' is a \code{list} and the desired output is a \code{list} than \code{obscovslist} should also be a \code{list} of \code{lists}
-#' with one \code{list} of observation covariates for each park.
-#' 
-#' @param output   Either "umf" (the defautlt) or "list". Controls the output when \code{object} is a list. If "umf" the data from all \code{NCRNbirds}
-#' objects in the list will be combined into a single unmarked frame. If "list" then the output will be a list of unmarked frames - one for 
-#' each \code{NCRNbirds} object in the input list. 
-#' @param ... Additonal arguments passed to \code{\link{CountXVisit}} and from there to \code{\link{getBirds}}.
-#' 
-#' @return Either an \code{unmarkedFrameOccu} or an \code{unmarkedFramePcount} object. 
-#' 
-#' @details This function simplifies the process for fitting a N-mixture model (Royal 2004, Kery et al. 2005) to data in an NCRNbirds object or a list of such objects. The data is first extracted from the object(s) using the \code{\link{CountXVisit}} funciton. The data is then fed to the \code{\link[unmarked]{pcount}} function and the resutls of the analysis are returned. 
+#' @details This function simplifies the process unmarked models (Royal 2004, Kery et al. 2005) to data in an NCRNbirds object or a list of such objects.
+#' The data is preppared by first using the \code{\link{makeUMF}} function to make the proper type of \code{unmarkedFrame}. The fraame can then be fed into
+#' \code(unmarkedBirds) for analysis. This function will automatically detect the frame type and do the corresponding analysis. If a formula is not supplied, 
+#' then all visit covariates in the unmarkedFrame will be used as detection covariates, and all site covaritates will be used as state (abundance or occupancy) 
+#' by defaults. 
 #' 
 #' @references Kery, M. Royle, J.A. and Schmid. 2005. Modeling avian abundance from replicated counts using binomail mixutre models. Ecological Applications. 15: 1450-1461. 
 #' @references Royle, J. A.  2004. N-Mixture models for estimating population size from spatially replicated counts. Biometrics 60: 108-115.
@@ -47,68 +31,15 @@
 
 ########################
 
-setGeneric(name="unmarkedBirds",function(object,formula=NULL, parks=NA, points=NA, AOU=NA, years=NA, times=NA, visits=NA, reps=NA, frametype="pcount",sitecovs=NULL, visitcovs=NA, 
-                                   obscovslist=NULL, output="umf",... ){standardGeneric("unmarkedBirds")},  signature="object")
+setGeneric(name="unmarkedBirds",function(object,formula=NULL, ...){standardGeneric("unmarkedBirds")},  signature="object")
 
-# setMethod(f="unmarkedBirds", signature=c(object="list"),
-#   function(object, parks, points, AOU, years, times, visits, reps, frametype, sitecovs, visitcovs, obscovslist, output, ...) {
-#     switch(output,
-#       umf= {
-#             BirdMat<-CountXVisit(object=object, parks=parks, points=points, AOU=AOU, years=years, times=times, visits=visits, reps=reps,
-#                                  type=switch(frametype, pcount="count", occu="occupancy"),output="dataframe", ...) %>% 
-#             dplyr::select(-c(Admin_Unit_Code, Point_Name, Year))
-#                  
-#             VisitData<- if(!anyNA(visitcovs)){
-#               visitcovs %>% map(~CovsXVisit(object, parks=parks, points=points, years=years, times=times, visits=visits, reps=reps, 
-#                                             covs=.x, output="dataframe") %>% 
-#                             select(-Admin_Unit_Code,-Point_Name,-Year)) %>% 
-#                            setNames(visitcovs)
-#             }else NULL
-#             return(makeUMF(object=BirdMat,frametype=frametype, sitecovs=sitecovs, obscovslist = c(VisitData, obscovslist)))
-#       },
-#                
-#       list={
-#             umflist<-list(object, sitecovs, obscovslist)
-#             umflist<-umflist[!sapply(umflist, is.null)] # removes any null elements so pmap will work
-#             return(pmap(.l=umflist, makeUMF, parks=parks, points=points, years=years,times=times, visits=visits, reps=reps, frametype=frametype, 
-#                         visitcovs=visitcovs) )
-#       }
-#     )
-#    }
-#  )
-#  
-#  
-# setMethod(f="unmarkedBirds", signature=c(object="NCRNbirds"),
-#     function(object, parks, points, AOU, years, times, visits, reps, frametype, sitecovs, visitcovs, obscovslist, ...){
-#            
-#       BirdMat<-CountXVisit(object=object, parks=parks, points=points, AOU=AOU, years=years, times=times, visits=visits,
-#                            reps=reps,  type=switch(frametype, pcount="count", occu="occupancy"), ...) %>% 
-#         dplyr::select(-c(Admin_Unit_Code, Point_Name, Year))
-#       
-#       VisitData<- if(!anyNA(visitcovs)){
-#         visitcovs %>% map(~CovsXVisit(object, parks=parks, points=points, years=years, times=times, visits=visits, reps=reps, covs=.x) %>% 
-#                       select(-Admin_Unit_Code,-Point_Name,-Year)) %>% 
-#           setNames(visitcovs)
-#       }else NULL
-#  
-#    return(makeUMF(object=BirdMat, frametype=frametype,sitecovs=sitecovs, obscovslist = c(VisitData, obscovslist)))
-#   }
-#  )
-# 
-# setMethod(f="unmarkedBirds", signature=c(object="data.frame"),
-#   function(object, frametype, sitecovs, obscovslist){
-#         
-#     
-#           
-#     UMFrame<-switch(frametype,
-#                   pcount=unmarkedFramePCount(y=object, siteCovs=sitecovs, obsCovs=obscovslist),
-#                   occu=unmarkedFrameOccu(y=object, siteCovs=sitecovs, obsCovs=obscovslist)
-#                   )
-#     
-#     return(UMFrame)
-#     
-#   }
-# )
+
+setMethod(f="unmarkedBirds", signature=c(object="list"),
+          function(object, formula, ...) {
+            lapply(X=object, FUN=unmarkedBirds, formula=formula, ...)
+          }
+)
+
 
 
 setMethod(f="unmarkedBirds", signature=c(object="unmarkedFrameOccu"),
