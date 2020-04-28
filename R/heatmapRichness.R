723536425f62b9f3f8daeb811ad8bea99292bdc9
John Paul Schmit <john_schmit@nps.gov>
2020-04-28 12:12:53 -0400

    -v add_Zach_mgt 7 - finished heatmapRichness - closes #132 - closes #133

--- R/heatmapRichness.R
+++ R/heatmapRichness.R
@@ -13,14 +13,11 @@
 #' @param object An \code{NCRNbirds} object a \code{list} of such objects, or a \code{data.frame} like that produced by \code{\link{birdRichness}}.
 #' @param byPoint Defaults to \code{FALSE}. If \code{TRUE} heatamap will have one line for each point, otherwise will have one line for each park
 #' @param color A color that will correspond to the maximum species richness
-#' @param output Either "total" (the default) or "list". Only used when \code{object} is a \code{list}
 #' @param plot_title  Optional,  A title for the plot. 
 #' @param scale If \code{TRUE} will include a scale to the right of the heatmap
 #' @param labels If\code{TRUE} will label species richness values on the heatmap.
-
+#' @param output Either "total" (the default) or "list". Only used when \code{object} is a \code{list}
 #' @param plot Logical. Return plot \code{TRUE} (default) or data.frame \code{FALSE}. 
-
-
 #' @param ... Additional arguments passed to \code{\link{birdRichness}}
 
 
@@ -63,25 +60,25 @@ setMethod(f="heatmapRichness", signature=c(object="data.frame"),
     if(!plot) return(object)
     object<-object %>% mutate(ParkName=factor(ParkName)) %>% 
       {if(byPoint) mutate(.,Point_Name=factor(Point_Name)) else .}
-   
+
     maxRichness<-max(object$Richness)
     
     heatmapOut<-ggplot(data=object, aes(x=as.factor(Year), y=if(byPoint) Point_Name else ParkName, fill=Richness)) + 
       geom_raster() +
-       theme(panel.background = element_rect(fill="transparent"),
-             panel.border= element_rect(color="white", fill="transparent"),
-             axis.text.x = element_text(size=10),
-             axis.text.y = element_text(size=10),
-             title = element_text(size=12),
-             legend.key.width=unit(2,"lines"),
-             legend.key.height=unit(3,"lines"))+
-       scale_fill_gradient(low = "white", high = color, na.value="gray", guide=if (scale) 'colorbar' else FALSE,
-                           limits=c(0, ceiling(maxRichness)),
-                           breaks=c(round(seq(0,maxRichness, length.out=5),0)))+
-      scale_y_discrete(limits=rev(if(byPoint) object$Point_Name else object$ParkName))+
-      labs(x=element_blank(), y=element_blank())+
-      {if(labels) geom_text(aes(label = round(Richness, 0)), size = 3,color="black")}+
-      {if(!is.na(plot_title)) ggtitle(plot_title)}
+        theme(panel.background = element_rect(fill="transparent"),
+              panel.border= element_rect(color="white", fill="transparent"),
+              axis.text.x = element_text(size=10),
+              axis.text.y = element_text(size=10),
+              title = element_text(size=12),
+              legend.key.width=unit(2,"lines"),
+              legend.key.height=unit(3,"lines"))+
+        scale_fill_gradient(low = "white", high = color, na.value="gray", guide=if (scale) 'colorbar' else FALSE,
+                            limits=c(0, ceiling(maxRichness)),
+                            breaks=c(round(seq(0,maxRichness, length.out=5),0)))+
+       scale_y_discrete(limits=rev(if(byPoint) levels(object$Point_Name) else levels(object$ParkName)))+
+       labs(x=element_blank(), y=element_blank())+
+       {if(labels) geom_text(aes(label = round(Richness, 0)), size = 3,color="black")}+
+       {if(!is.na(plot_title)) ggtitle(plot_title)}
     
 print(heatmapOut)
     
