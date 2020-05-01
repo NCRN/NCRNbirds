934c8cea79f118fa9e9c28fb150459a0e6bd70c6
John Paul Schmit <john_schmit@nps.gov>
2020-05-01 13:54:47 -0400

    v add_Zach_mgt 9
    - barPlotRichness now works for lists and NCRNobjects
    
    - updated help file.

--- R/barplotRichness.R
+++ R/barplotRichness.R
@@ -10,19 +10,21 @@
 #' @description Makes a barplot of species richness
 #' 
 #' @param object An \code{NCRNbirds} object a \code{list} of such objects, or a \code{data.frame} like that produced by \code{\link{birdRichness}}.
-#' @param byPark 
-#' @param byPoint Defaults to \code{FALSE}. If \code{TRUE} heatamap will have one line for each point, otherwise will have one line for each park
-#' @param color A color that will correspond to the maximum species richness
+#' @param byPark Defaults to \code{FALSE}. If true the barplot will have on bar for each park. 
+#' @param byYear Defaults to \code{FALSE}. If true the barplot will have on bar for each year. 
+#' @param byPoint Defaults to \code{FALSE}. If \code{TRUE} the barplot will have one bar for each point.
+#' @param color A color for the bars. 
 #' @param plot_title  Optional,  A title for the plot. 
-#' @param scale If \code{TRUE} will include a scale to the right of the heatmap
-#' @param labels If\code{TRUE} will label species richness values on the heatmap.
+#' @param scale If \code{TRUE} will include a scale to the right of the barplot. 
+#' @param labels If\code{TRUE} will label species richness values on the barplot.
 #' @param output Either "total" (the default) or "list". Only used when \code{object} is a \code{list}
 #' @param plot Logical. Return plot \code{TRUE} (default) or data.frame \code{FALSE}. 
 #' @param ... Additional arguments passed to \code{\link{birdRichness}}
 
 
-#' @details This function produces a heatmap of species richness at the park or point level across years. It does this by using the 
-#' output of the \code{\link{birdRichness}} function. The data is then passed on to ggplot2 for graphing.
+#' @details This function produces a barplot of species richness by Park, Year or Point. It does this by using the 
+#' output of the \code{\link{birdRichness}} function. The data is then passed on to ggplot2 for graphing. The user can chose to have \code{byPark=T}, 
+#' \code{byYear=T} or \code{byPoint=T}, but should only pick one of the three. 
 #'   
 #' @export
 
@@ -30,39 +32,40 @@
 setGeneric(name="barplotRichness",function(object, byPark=F, byYear=F, byPoint=F, color="dark green", plot_title=NA, scale=T, labels=T, output="total",
                                            plot=TRUE, ...){standardGeneric("barplotRichness")}, signature="object")
 
-# 
-# setMethod(f="barplotRichness", signature=c(object="list"),
-#    function(object, byPoint, color, plot_title, scale, labels, output, plot,  ...) {
-#      switch(output,
-#        total={
-#           heatmapdata=birdRichness(object,byPark=T, byYear=T, byPoint=byPoint, output="total",...)
-# 
-#         return(heatmapRichness(object=heatmapdata, byPoint=byPoint, color=color, plot_title=plot_title, scale=scale, labels=labels, plot = plot))
-#       },
-#        list={
-#          return(map(object,heatmapRichness, byPoint=byPoint, color=color, plot_title=plot_title, scale=scale, labels=labels, plot=plot, ...))
-#        }
-#      )
-#  })
-#  
-#  setMethod(f="barplotRichness", signature=c(object="NCRNbirds"),
-#    function(object, byPoint, color, plot_title, scale, labels, plot, ...){
-#   heatmapdata<-birdRichness(object, byPark=T, byYear=T, byPoint=byPoint, ...)
-#   
-#   heatmapRichness(object=heatmapdata, byPoint=byPoint, color=color, plot_title = plot_title, scale=scale, labels=labels, plot=plot)
-# 
-#  
-#  })
+ setMethod(f="barplotRichness", signature=c(object="list"),
+    function(object, byPark, byYear, byPoint, color, plot_title, scale, labels, output, plot,  ...) {
+      switch(output,
+        total={
+           barplotdata=birdRichness(object,byPark=byPark, byYear=byYear, byPoint=byPoint, output="total",...)
+ 
+         return(barplotRichness(object=barplotdata, byPark=byPark, byYear=byYear, byPoint=byPoint, color=color, plot_title=plot_title,
+                                scale=scale, labels=labels, plot = plot))
+       },
+        list={
+          return(map(object,barplotRichness,byPark=byPark, byYear=byYear,  byPoint=byPoint, color=color, plot_title=plot_title, 
+                     scale=scale, labels=labels, plot=plot, ...))
+        }
+      )
+  })
+  
+  setMethod(f="barplotRichness", signature=c(object="NCRNbirds"),
+    function(object, byPark, byYear, byPoint, color, plot_title, scale, labels, plot, ...){
+   barplotdata<-birdRichness(object, byPark=byPark, byYear=byYear, byPoint=byPoint, ...)
+   
+   barplotRichness(object=barplotdata, byPark=byPark, byYear=byYear, byPoint=byPoint, color=color, plot_title = plot_title, scale=scale, 
+                   labels=labels, plot=plot)
+
+  })
 
 setMethod(f="barplotRichness", signature=c(object="data.frame"),
   function(object, byPark, byYear, byPoint, color, plot_title, scale, labels, plot){
     
-    if(!plot) return(object)
-    
      object<-object %>% 
-       {if(byPark) mutate(., ParkName=factor(ParkName))  else .}%>% 
-       {if(byYear) mutate(., Year=factor(Year))  else .}%>% 
+       {if(byPark) mutate(., ParkName=factor(ParkName))  else .} %>% 
+       {if(byYear) mutate(., Year=factor(Year))  else .} %>% 
        {if(byPoint) mutate(.,Point_Name=factor(Point_Name)) else .} 
+     
+     if(!plot) return(object)
      
      xcol<-sym(case_when(byPark~"ParkName", byYear~"Year", byPoint~"Point_Name"))
 # 
@@ -109,9 +112,9 @@ ggplot(data=object,aes(x=!!xcol, y=Richness))+
   scale_x_discrete(limits=rev(levels(object %>% select(!!xcol))))+
   #scale_fill_manual(values=mycolors)+
   labs(x=element_blank(),y="Species Richness")+
-  geom_text(aes(x=!!xcol, y=Richness, label=Richness)#, group=Type)
-            ,hjust = 1.3, 
-            color="white", size = 3.5, fontface="bold",inherit.aes = TRUE)+
-  coord_flip()
+  {if(labels) geom_text(aes(x=!!xcol, y=Richness, label=Richness),# group=Type)
+            hjust = 1.3, color="white", size = 3.5, fontface="bold",inherit.aes = TRUE)}+
+  {if(!is.na(plot_title)) ggtitle(plot_title)}+
+  {if(byYear) coord_flip()}
     
 })
