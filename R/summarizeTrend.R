dcbe0cc75694de035f7d76f5f7bb1cd938e264b9
John Paul Schmit <john_schmit@nps.gov>
2020-06-19 16:38:10 -0400

    v add Zach mgt 15
    - much improved table
    - estimates both Z and psi
    - works for numerical trends too!

--- R/summarizeTrend.R
+++ R/summarizeTrend.R
@@ -5,17 +5,22 @@
 #' @description Analyzes an unmarkedFrame object using methods from the unmarked package.
 #' 
 #' @importFrom boot inv.logit
-#' @importFrom dplyr group_by mutate summarize
+#' @importFrom dplyr arrange group_by mutate summarize
 #' @importFrom magrittr %>%
 #' @importFrom unmarked bup coef confint getData getY predict ranef SE show siteCovs
-
 #' @importFrom purrr map pmap
-
-
-#' @param object An unamked frame object such as those created by \code{makeUMF} or a \code{list} of such frames. 
-#' @return A table which summarizes the trend analysis results. Currently fits occupancy and n-mixture models. 
-#' @details This function simplifies the process of extracting trends over tiem from unmarked models (Royal 2004, Kery et al. 2005) generated
-#' by the \code{\link{unmarkedBirds}} command. The code currenlty assumes you have an occupancy model fit with the trend Year as a factor. 
+#' 
+#' @param object An unmarked fit object such as those created by \code{unmarkedBirds} or a \code{list} of such frames. 
+#' @return A table which summarizes the trend analysis results. Currently fits occupancy models with only year (factor or numeric) as a site covariate. 
+#' @details This function simplifies the process of extracting trends from unmarked models (Royal 2004, Kery et al. 2005) generated
+#' by the \code{\link{unmarkedBirds}} function. 
+#' 
+#' The function will return a table, where each row is a differnet year. Columns include, the year, the number of points visited. naive 
+#' occupancy for each year, the model coefficient and standard error. The function also returns two sets of estimates of the true occupancy for each year.
+#' The first, \emph{psi}, is the inverse logit of the model coefficient and represents the estimated occupancy of a site given its site and visit covariates. 
+#' The second estimate, \emph{z}, takes into account not only covariates but also the actual detection history for each site.  Each estimate comes with 
+#' its own upper and lower limits for a 95\% CI. Note that as \emph{z} takes into account the actual detection history, its lower limit is 
+#' always at least the naive occupancy, whereas \emph{psi} has no such lower boundary.
 #'  
 #' 
 #' @export
@@ -37,21 +42,26 @@ setGeneric(name="summarizeTrend",function(object,tabletype="state", ...){standar
 setMethod(f="summarizeTrend", signature=c(object="unmarkedFitOccu"),
           function(object,tabletype,...){
 
-  ModelType<-if(class(siteCovs(getData(object))$Year)=="factor") "factor" else "numeric"
+  ModelType<-if(all(class(siteCovs(getData(object))$Year)=="factor")) "factor" else "numeric"
  
   ModelData<-getData(object)
  
   
   InFrame<-as.data.frame(getY(ModelData))
-  InFrame<-cbind(InFrame, siteCovs(ModelData))%>% mutate(Occupied = Visit1 | Visit2) %>% 
-   group_by(Year) %>% summarize(Sites=n(), Occupancy=sum(Occupied/Sites, na.rm=T))
 
+  InFrame<-cbind(InFrame, siteCovs(ModelData))%>% mutate(Occupied = Visit1 | Visit2) %>% 
+   group_by(Year) %>% summarize(Points=n(), Occupancy=sum(Occupied/Points, na.rm=T))
+
+  
   ModelRanef<-ranef(object)
+
   RanefMode<-bup(ModelRanef, stat="mean")
   ModeCI<-confint(ModelRanef, level=0.95)
   
-  Estimates<-cbind(data.frame(Mode=RanefMode),ModeCI, siteCovs(ModelData)) %>% 
+  Estimates<-cbind(data.frame(Mode=RanefMode),ModeCI, as.data.frame(siteCovs(ModelData))) %>% 
     group_by(Year) %>% summarize(Estimate=round(mean(Mode),3), Lower=round(mean(`2.5%`),3),Upper=round(mean(`97.5%`),3))
+  
+  
  # StatePreds<-cbind(predict(object,'state'), siteCovs(ModelData)) %>% 
 #   group_by(Year) %>% summarize(Preidct=round(mean(Predicted),3), Lower=round(mean(lower),3),Upper=round(mean(upper),3))
 
@@ -61,26 +71,35 @@ setMethod(f="summarizeTrend", signature=c(object="unmarkedFitOccu"),
 
 
  
-OutTable<-data.frame(Year=if(ModelType=="factor") levels(siteCovs(ModelData)$Year) else "All" ,
-                  Sites=InFrame$Sites,
-                  Naive_Occu=round(InFrame$Occupancy,2),
-                  Coef=c(coef(object, type="state")[1],coef(object, type="state")[1]+coef(object, type="state")[-1]),
-                  Estimate=Estimates$Estimate,
-                  Lower95=Estimates$Lower,
-                  Upper95=Estimates$Upper
+OutTable<-data.frame(Year=if(ModelType=="factor") levels(siteCovs(ModelData)$Year) else Estimates$Year,
+                    Points=InFrame$Points,
+                    Naive_Occu=round(InFrame$Occupancy,2),
+                    Coef=switch (ModelType,
+                      factor = c(coef(object, type="state")[[1]],coef(object, type="state")[[1]]+coef(object, type="state")[-1]) %>% round(2),
+                      numeric = (coef(object,"state")[[1]]+coef(object, "state")[[2]]*Estimates$Year) %>% round(2)
+                    ),
+                    SE=switch(ModelType,
+                       factor= c( SE(object, type="state")[1], sqrt( (SE(object, type="state")[1]^2)+ (SE(object, type="state")[-1]^2))) %>% round(2),
+                       numeric=sqrt((SE(object, type="state")[[1]]^2)+(SE(object, type="state")[[2]]^2)) %>% round(2)
+                    ),
+                   z=Estimates$Estimate %>% round(2),
+                   z_lower95=Estimates$Lower %>% round(2),
+                   z_upper95=Estimates$Upper %>% round(2)
 
 )
 
+ OutTable<-OutTable %>% mutate(psi=inv.logit(Coef) %>% round(2), psi_lower95=inv.logit(Coef-2*SE) %>% round(2), 
+                               psi_upper95=inv.logit(Coef+2*SE) %>% round(2)) %>% 
+   arrange(Year, Points, Naive_Occu, Coef, SE, psi, psi_lower95, psi_upper95, z, z_lower95, z_upper95)
+
+ OutTable$Year<-if(ModelType=="numeric") OutTable$Year + attr(siteCovs(ModelData)$Year,"scaled:center") else OutTable$Year
+    
 rownames(OutTable)<-c()
+
 return(OutTable)
 
 
-#States
-#If list a model nam  e          
-            # naive occupancy
-# sites
-# estimated occupanyc
-# SE
+
 
             
 # Detection table 
@@ -89,13 +108,7 @@ return(OutTable)
 # estiamted detection 
 # se
 
-            
-#Site Table?
-#if list a model name
-#Visits
-#naive estiamte
-# probablity of occupnayc
-# visit specific detectability
+
             
         
 
